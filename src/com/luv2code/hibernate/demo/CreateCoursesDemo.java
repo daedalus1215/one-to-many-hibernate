8e366b87521a335486369a74501f840397f580f9
larry <ladams1776@yahoo.com>
2018-05-21 06:54:43 -0400

    cleaned up the main void method to get ready to add the courses.

---
+++ src/com/luv2code/hibernate/demo/CreateCoursesDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+
+public class CreateCoursesDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		
+			
+		try {
+			session.beginTransaction();
+
+			Course tempCourse = new Course();
+			
+			
+			
+			
+			
+			session.save(tempInstructor);			
+			// commit
+			session.getTransaction().commit();
+			
+		} catch(Exception exception) {
+			exception.printStackTrace();
+		} finally {			
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
