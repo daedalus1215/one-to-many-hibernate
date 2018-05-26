995afa04e962c149cb8c1937815da8f54d479f76
larry <ladams1776@yahoo.com>
2018-05-26 08:10:15 -0400

    Deleting a course.

---
+++ src/com/luv2code/hibernate/demo/DeleteCourseDemo.java
@@ -0,0 +1,54 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+
+public class DeleteCourseDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		
+			
+		try {
+			// transaction begins
+			session.beginTransaction();
+
+
+			// get a course
+			int courseID = 2;
+			Course deletableCourse = session.get(Course.class, courseID);
+			
+
+			System.out.println("Deleteing course: " + deletableCourse);
+			
+			// delete course
+			session.delete(deletableCourse);
+			
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			
+		} catch(Exception exception) {
+			exception.printStackTrace();
+		} finally {			
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
